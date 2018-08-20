4a7617ccc9d96f006091e7b4907b17fcb514daf2
337340028@qq.com <https://github.com/aimaole/maomao>
2018-08-20 18:09:27 +0800

    test

--- bigdata/hadoop/src/main/java/test/test.java
+++ bigdata/hadoop/src/main/java/test/test.java
@@ -1,14 +1,13 @@
 package test;
 
+import java.util.Random;
+
 public class test {
 
 	public static void main(String[] args) {
-
-		Integer i = 10;
-
-		for (int j = 1; j < i; j++) {
-			System.out.println("{\"id\":"+j+",\"label\":\"person\",\"inE\":{\"knows\":[{\"id\":7,\"outV\":1,\"properties\":{\"weight\":0.5}}]},\"properties\":{\"name\":[{\"id\":2,\"value\":\"vadas\"}],\"age\":[{\"id\":3,\"value\":27}]}}");
-		}
+		Random  ra = new Random();
+		int a  =  ra.nextInt(100);
+		System.out.println(a);
 
 	}
 
--- sparktest/JSqlParser-test/src/main/java/com/maomao/test.java
+++
@@ -1,7 +0,0 @@
-package com.maomao;
-
-public class test {
-    public static void main(String[] args) {
-        System.out.println("hello.world");
-    }
-}
