d54ceac6cf37bb261c426c6a03b75fde36449546
337340028@qq.com <337340028@qq.com>
2019-04-15 22:38:13 +0800

    反射及类加载

---
+++ bigdata/hadoop/src/main/java/reflect/TestMain.java
@@ -0,0 +1,29 @@
+package reflect;
+
+public class TestMain {
+    public static void main(String[] args) {
+        System.out.println(XYZ.name);
+
+        // 不会初始化静态块
+        Class clazz1 = Base.class;
+        System.out.println("------");
+        // 会初始化
+        try {
+            Class clazz2 = Class.forName("reflect.Base");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class XYZ {
+    public static String name = "luoxn28";
+
+    static {
+        System.out.println("xyz静态块");
+    }
+
+    public XYZ() {
+        System.out.println("xyz构造了");
+    }
+}
