660675bde5de08eeb1a33d710eccdfad7f6ae63a
337340028@qq.com <https://github.com/aimaole/maomao>
2019-06-13 18:55:02 +0800

    sort

--- bigdata/hadoop/src/main/java/com/test/Test2.java
+++ bigdata/hadoop/src/main/java/com/test/Test2.java
@@ -6,23 +6,13 @@ package main.java.com.test;
 public class Test2 {
     public static void main(String[] args) {
         int[] arr = {3, 1, 2, 4, 5, 9, 8};
-        sort(0, arr.length - 1, arr);
+        mergesort(arr);
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
         }
     }
 
-    public static void sort(int low, int high, int[] arr) {
-        int i;
-        int tmp;
-        for (i = 0; i < arr.length-2; i++) {
-            if (arr[i] > arr[i + 1]) {
-                tmp = arr[i];
-                arr[1] = arr[i + 1];
-                arr[i + 1] = tmp;
-            }
+    public static void mergesort(int[] arr) {
 
-        }
-        return;
     }
 }
